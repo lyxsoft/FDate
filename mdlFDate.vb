8ffaf1ce1768c950c4eb9544c9b70a8cc78fd95c
YX.Li <lyxsoft@users.noreply.github.com>
2019-04-21 10:29:09 +0800

    Initial commit

---
+++ mdlFDate.vb
@@ -0,0 +1,22 @@
+﻿Imports System
+Imports System.IO
+
+Module mdlFDate
+
+    Sub Main()
+        If My.Application.CommandLineArgs.Count = 2 Then
+            If IsDate(My.Application.CommandLineArgs(1)) Then
+                Try
+                    File.SetCreationTime(My.Application.CommandLineArgs(0), My.Application.CommandLineArgs(1))
+                    File.SetLastWriteTime(My.Application.CommandLineArgs(0), My.Application.CommandLineArgs(1))
+                    Console.WriteLine(My.Application.CommandLineArgs(0) & " set to " & Format(CDate(My.Application.CommandLineArgs(1)), "yyyy-MM-dd HH:mm"))
+                Catch ex As FileNotFoundException
+                    Console.WriteLine("File not found: " & My.Application.CommandLineArgs(0))
+                Catch ex As Exception
+                    Console.WriteLine(ex)
+                End Try
+            End If
+        End If
+    End Sub
+
+End Module
